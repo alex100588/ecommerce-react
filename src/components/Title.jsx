1ee40a88bcdd7bcd9c7e3761c7a54660ccbbf1f8
alex100588 <49831245+alex100588@users.noreply.github.com>
2025-03-09 11:48:55 +0200

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,12 @@
 const Title = () => {
+
   return (
-    <h1 className="text-center mb-4">
-      <p className="text-title text-white"> Alex PetShop </p>
-    </h1>
+    <div className="container">
+      <h1 className="text-center mb-4">
+        <p className="text-title text-white"> Alex PetShop </p>
+      </h1>
+      <img className="w-100 mb-3 title"  src={`${process.env.PUBLIC_URL}/img/shop5.jpg`} alt="" />
+    </div>
   );
 };
 
